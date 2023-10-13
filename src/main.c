7f1923e0f8d1ad235a4eb1a70af15b7d4375b3ce
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-13 16:52:19 -0700

    its fake security now

--- src/main.c
+++ src/main.c
@@ -1,21 +1,38 @@
+#include <string.h>
+#include <stdio.h>
 #include <ti/screen.h>
 #include <ti/getcsc.h>
-#include <stdlib.h>
+#include <sys/lcd.h>
+#include <sys/timers.h>
 
-int main()
+void FillScreen(uint8_t color)
 {
-    // clear home screen
+    memset(lcd_Ram, color, LCD_SIZE);
+}
+
+int main(void)
+{
+    char passcode[5];
     os_ClrHome();
+    sprintf(passcode, "%s", "5678");
 
-    // print my string
-    os_PutStrFull("Hello, World.");
-    os_NewLine();
-    os_PutStrFull("what if this printed formulas or something");
+    char inputBuffer[5];
+    char response[5];
 
-    // while no key is pressed, do nothing
-    // i.e. wait for key press
-    while (!os_GetCSC());
+    os_GetStringInput("enter passcode: ", inputBuffer, 5);
+    sprintf(response, "%s", inputBuffer);
 
-    // return
-    return 0;
+    FillScreen(0x00);
+    msleep(1000);
+
+    if (!strcmp(passcode, response)) {
+        os_ClrHome();
+        return 0;
+    } else {
+        FillScreen(0xFF);
+        os_NewLine();
+        os_PutStrFull("no.");
+        msleep(1000);
+        main();
+    }
 }
